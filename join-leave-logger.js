2ea08664174adb226b2e5f143648e8ce16580810
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-07-06 07:39:33 +0900

    added license

--- join-leave-logger.js
+++ join-leave-logger.js
@@ -1,5 +1,15 @@
 import { world } from 'mojang-minecraft';
 
+/**
+ * Join-Leave Logger
+ * @license MIT
+ * @author tutinoko2048 / RetoRuto9900K
+ * @version 1.0.0
+ * --------------------------------------------------------------------------
+ * コンテンツログにプレイヤーの参加退出ログを記録します
+ * --------------------------------------------------------------------------
+ */
+
 world.events.playerJoin.subscribe(ev => {
   const { player } = ev;
   console.log(`Join >> ${player.name}`);
