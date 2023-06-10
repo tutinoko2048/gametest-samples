59ddfbdb70a3652d60f4ea335fd2a846510b6169
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-06-11 08:19:28 +0900

    Update: v1.20.0

--- samples/door.js
+++ samples/door.js
@@ -1,16 +1,17 @@
 // @ts-check
 
+
 import { world } from '@minecraft/server';
 /**
  * Automatic Double swing Door
  * @license MIT
  * @author tutinoko2048 / RetoRuto9900K
- * @version 1.19.80
+ * @version 1.20.0
  * --------------------------------------------------------------------------
  * 片方のドアを開くと反対側のドアも開きます
  * --------------------------------------------------------------------------
  */
-
+ 
 const DOOR = [
   'minecraft:acacia_door',
   'minecraft:birch_door',
@@ -23,36 +24,35 @@ const DOOR = [
   'minecraft:wooden_door'
 ];
 
-world.events.itemUseOn.subscribe(ev => {
-  const { source: { dimension } } = ev;
-  const location = ev.getBlockLocation();
-  const block = dimension.getBlock(location);
+world.afterEvents.itemUseOn.subscribe(ev => {
+  const { source: { dimension }, block } = ev;
+  const location = block.location;
   if (!DOOR.includes(block.typeId)) return;
-  const isUpper = block.permutation.getProperty('upper_block_bit');
+  const isUpper = block.permutation.getState('upper_block_bit');
   if (isUpper === null) return;
   
   const under = { ...location, y: location.y - 1 };
   
   const isOpen = isUpper
-    ? dimension.getBlock(under).permutation.getProperty('open_bit')
-    : block.permutation.getProperty('open_bit')
+    ? dimension.getBlock(under).permutation.getState('open_bit')
+    : block.permutation.getState('open_bit')
   
   const direction = /** @type { number } */ (isUpper
-    ? dimension.getBlock(under).permutation.getProperty('direction')
-    : block.permutation.getProperty('direction'))
+    ? dimension.getBlock(under).permutation.getState('direction')
+    : block.permutation.getState('direction'))
 
   if (isOpen === null || direction === null) return;
   
   for (const loc of getNearLocations(isUpper ? under : location, direction)) {
     const _block = dimension.getBlock(loc);
     const _permutation = _block.permutation.clone();
-    const _isOpen = _permutation.getProperty('open_bit'); // ドア操作前の値
-    const _direction = _permutation.getProperty('direction');
+    const _isOpen = _permutation.getState('open_bit'); // ドア操作前の値
+    const _direction = _permutation.getState('direction');
     
     if (block.typeId != _block.typeId || _isOpen === null || _direction === null) continue;
     if (direction != _direction) continue;
     
-    const newPermutation = _permutation.withProperty('open_bit', !isOpen);
+    const newPermutation = _permutation.withState('open_bit', !isOpen);
     _block.setPermutation(newPermutation);
   }
 });
